37591e859c0fb5a5069fa6da8ec364200e27bda9
AnthonyMaddox <anthonyjmaddox@gmail.com>
2021-02-11 11:34:59 -0500

    deployed backend seperately

--- src/ContactForm.js
+++ src/ContactForm.js
@@ -11,7 +11,7 @@ const ContactForm = () => {
       email: email.value,
       message: message.value,
     };
-    let response = await fetch("https://react-email.herokuapp.com/contact", {
+    let response = await fetch("https://react-email-backend.herokuapp.com/contact", {
       method: "POST",
       headers: {
         "Content-Type": "application/json;charset=utf-8",
